99933983ff057119ae84d4293125563079b35c71
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:16:01 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"The baby _____ the living room floor with colour pens while her mother _____ the kitchen.",
+        options:["marked / has been mopping", "has marked / mopped", "was marking / was mopping", "had marked / had mopped"],
+        answer: 2
+    },
+    {
+     q:"There _____ some ketchup around his mouth because he _____ spaghetti.",
+        options:["has been / eats", "had been / has eaten", "is / had been eating", "was / had been eating"],
+        answer: 3   
+    },
+    {
+        q:"Because I _____ to eating with chopsticks, I _____ a hard time during the dinner.",
+        options:["wasn't used / had", "am not used / had been having", "hadn't been used / am having", "didn't use / was having"],
+        answer: 0
+    },
+    {
+        q:"When the film finished, I _____ the alarm clock and _____ to bed.",
+        options:["had sate / have gone", "set / went", "was setting / was going", "had set / is going"],
+        answer: 1
+    },
+    {
+        q:"Janette _____ all her friends for advice before she _____ to quit her job.",
+        options:["was asking / has decided", "has asked / decides", "had asked / decided", "has been asking / had decided"],
+        answer: 2
+    }
+]
